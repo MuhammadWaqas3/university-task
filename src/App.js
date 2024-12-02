5b61fc654637a1992f8008cf2a56c9d5bcfdbde7
unknown <MuhammadWaqas3>
2024-12-02 11:16:31 +0500

    task-1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import './App.css'; // Import global styles
-import Navbar from '../src/components/landingPage/navbar';
-import LandingPage from '../src/components/landingPage/header';
-import Form from './components/from';
-import Footer from './components/landingPage/footer';
+import Profile from './components/profileCard';
+
 
 function App() {
 return (
 <div>
-<Navbar />
-<LandingPage />
-<h1 className='form-heading'>Get Expert Help for Your Laptop Today!</h1>
-<Form/>
-<Footer/>
+
+<Profile name={"Waqas"} title={"Software Engineer"}  age={21} location={"Karachi"} />
+
 </div>
 );
 }
