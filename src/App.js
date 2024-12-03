f41e81196e7e8cab8ae515f23c1d8a11d82d3d5b
Muhammad Waqas3 <muhammad.waqas.56964@gmail.com>
2024-12-03 23:17:44 +0500

    lab08 task completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,113 @@
+// //
+
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// import "./App.css";
+// import Product from "./components/productCard/product";
+// import Profile from "./components/profileCard/profile";
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="App">
+//         {/* Define Routes */}
+//         <Routes>
+//           <Route
+//             path="./product.js"
+//             element={
+//               <div>
+//                 <Product
+//                   id={12345}
+//                   name="iPhone 16 Pro"
+//                   price="$899.00"
+//                   url="./product-image.svg"
+//                 />
+//                 {/* Add Link to Profile */}
+//                 <div className="link-to-profile">
+//                   <Link to="./profile.js">Click to watch my profile →</Link>
+//                 </div>
+//               </div>
+//             }
+//           />
+//           <Route
+//             path="/profile"
+//             element={
+//               <div>
+//                 <Profile
+//                   name="Waqas"
+//                   title="Software Engineer"
+//                   age={21}
+//                   location="Karachi"
+//                 />
+//                 {/* Add Link to Product */}
+//                 <div className="link-to-product">
+//                   <Link to="/">Click to watch my product →</Link>
+//                 </div>
+//               </div>
+//             }
+//           />
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
+
 import React from "react";
-import "./App.css"; // Import global styles
-// import Profile from "./components/profileCard";
-import Product from "./components/productCard";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Product from "./components/productCard/product";
+import Profile from "./components/profileCard/profile";
 
 function App() {
   return (
-    <div>
+    <Router>
+      <div className="App">
+        {/* Define Routes */}
+        <Routes>
+         
+          <Route
+            path="/profile"
+            element={
+              <div>
+                <Profile
+                  name="Waqas"
+                  title="Software Engineer"
+                  age={21}
+                  location="Karachi"
+                />
+                {/* Add Link to Product */}
+                <div className="link-to-product">
+                  <Link to="/">Click to watch my product →</Link>
+                </div>
+              </div>
+            }
 
-      {/* <Profile name={"Waqas"} title={"Software Engineer"}  age={21} location={"Karachi"} /> */}
-      <Product id={12345} name={"iphone 16 pro"} price={"$899.00"}  url={"./product-image.svg"} />
-
-    </div>
+        
+          />
+ <Route
+            path="/"
+            element={
+              <div>
+                <Product
+                  id={12345}
+                  name="iPhone 16 Pro"
+                  price="$899.00"
+                  url="./product-image.svg"
+                />
+                {/* Add Link to Profile */}
+                <div className="link-to-profile">
+                  <Link to="/profile">Click to watch my profile →</Link>
+                </div>
+              </div>
+            }
+          />        </Routes>
+      </div>
+    </Router>
   );
 }
+
 export default App;
