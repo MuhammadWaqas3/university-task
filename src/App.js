8d66225bdc0ca4b171000203cb400771078d9f14
Waqas <125983247+MuhammadWaqas3@users.noreply.github.com>
2024-12-04 17:55:52 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,61 +1,3 @@
-// //
-
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-// import "./App.css";
-// import Product from "./components/productCard/product";
-// import Profile from "./components/profileCard/profile";
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="App">
-//         {/* Define Routes */}
-//         <Routes>
-//           <Route
-//             path="./product.js"
-//             element={
-//               <div>
-//                 <Product
-//                   id={12345}
-//                   name="iPhone 16 Pro"
-//                   price="$899.00"
-//                   url="./product-image.svg"
-//                 />
-//                 {/* Add Link to Profile */}
-//                 <div className="link-to-profile">
-//                   <Link to="./profile.js">Click to watch my profile →</Link>
-//                 </div>
-//               </div>
-//             }
-//           />
-//           <Route
-//             path="/profile"
-//             element={
-//               <div>
-//                 <Profile
-//                   name="Waqas"
-//                   title="Software Engineer"
-//                   age={21}
-//                   location="Karachi"
-//                 />
-//                 {/* Add Link to Product */}
-//                 <div className="link-to-product">
-//                   <Link to="/">Click to watch my product →</Link>
-//                 </div>
-//               </div>
-//             }
-//           />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
@@ -66,7 +8,6 @@ function App() {
   return (
     <Router>
       <div className="App">
-        {/* Define Routes */}
         <Routes>
          
           <Route
@@ -79,7 +20,6 @@ function App() {
                   age={21}
                   location="Karachi"
                 />
-                {/* Add Link to Product */}
                 <div className="link-to-product">
                   <Link to="/">Click to watch my product →</Link>
                 </div>
@@ -98,7 +38,6 @@ function App() {
                   price="$899.00"
                   url="./product-image.svg"
                 />
-                {/* Add Link to Profile */}
                 <div className="link-to-profile">
                   <Link to="/profile">Click to watch my profile →</Link>
                 </div>
